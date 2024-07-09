4f6f1c4c58962e6fec200acbce6dc49a7afd1420
Owi27 <owens.nicolas02@icloud.com>
2024-07-09 12:37:08 -0400

    RGB to hex working and rendering

---
+++ Renderer.h
@@ -0,0 +1,51 @@
+class Renderer
+{
+	// proxy handles
+	GW::SYSTEM::GWindow _win;
+	GW::GRAPHICS::GRasterSurface _ras;
+	// what we need at a minimum to draw a triangle
+	float verts[6] =
+	{
+		   0,   0.5f,
+		 0.5f, -0.5f,
+		-0.5f, -0.5f
+	};
+public:
+	Renderer(GW::SYSTEM::GWindow win, GW::GRAPHICS::GRasterSurface ras)
+	{
+		_win = win;
+		_ras = ras;
+	}
+
+	void Render(unsigned int* xrgb, unsigned int w, unsigned int h)
+	{
+		for (size_t y = 0; y < h; y++)
+		{
+			for (size_t x = 0; x < w; x++)
+			{
+				auto r = double(x) / (w - 1);
+				auto g = double(y) / (h - 1);
+				auto b = 0.0;
+
+				unsigned int ir = (int)255.999 * r;
+				unsigned int ig = (int)255.999 * g;
+				unsigned int ib = (int)255.999 * b;
+
+				unsigned int tem = 0xFF | 0xFF >> 8 | 0xFF >> 16 | 0x00 >> 24;
+				unsigned int temp = 0xFF | (unsigned int)ir | (unsigned int)ig | (unsigned int)ib;
+				xrgb[y * w + x] = RGBtoHEX(0xFF, ir, ig, ib);
+			}
+		}
+	}
+
+	~Renderer()
+	{
+		// nothing to delete
+	}
+
+private:
+	unsigned int RGBtoHEX(unsigned int x, int r, int g, int b)
+	{
+		return ((x & 0xff) << 24) + ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+	}
+};
--- pch.h
+++ pch.h
@@ -1,1 +1,12 @@
-#include <iostream>
+// Simple basecode showing how to create a window and attach a GRasterSurface
+#define GATEWARE_ENABLE_CORE // All libraries need this
+#define GATEWARE_ENABLE_SYSTEM // Graphics libs require system level libraries
+#define GATEWARE_ENABLE_GRAPHICS // Enables all Graphics Libraries
+// Ignore some GRAPHICS libraries we aren't going to use
+#define GATEWARE_DISABLE_GDIRECTX11SURFACE // we have another template for this
+#define GATEWARE_DISABLE_GVULKANSURFACE // we have another template for this
+#define GATEWARE_DISABLE_GDIRECTX12SURFACE // we have another template for this
+#define GATEWARE_DISABLE_GOPENGLSURFACE // we have another template for this
+// With what we want & what we don't defined we can include the API
+#include "Gateware/Gateware.h"
+#include "Renderer.h"
